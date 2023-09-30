27634d951d81867c80ebbb769c5e8a6f20fe53ca
Jakub Pawłowski <jakubpawlowski188@gmail.com>
2023-09-30 23:12:10 +0200

    Added minimap folow, bollet traces, muzzle flash and minimap zoom

--- Assets/Scripts/Camera/minimapCameraMovement.cs
+++ Assets/Scripts/Camera/minimapCameraMovement.cs
@@ -1,15 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
-public class minimapCameraMovement : MonoBehaviour
+public class MinimapCameraMovement : NetworkBehaviour
 {
-    public GameObject player;
+    private Transform playerTransform; // Cache the player's transform instead of the whole GameObject.
+
+    private void Start()
+    {
+        // Initialize the playerTransform in Start to avoid unnecessary FindGameObjectsWithTag calls.
+        FindAndSetPlayer();
+    }
 
     private void LateUpdate()
     {
-        transform.position = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
+        if (playerTransform != null)
+        {
+            // Use playerTransform instead of player GameObject for performance.
+            Vector3 newPosition = new Vector3(playerTransform.position.x, transform.position.y, playerTransform.position.z);
+            transform.position = newPosition;
 
-        transform.rotation = Quaternion.Euler(90f, player.transform.eulerAngles.y, 0f);
+            // You can directly access the player's rotation.
+            transform.rotation = Quaternion.Euler(90f, playerTransform.eulerAngles.y, 0f);
+        }
+    }
+
+    private void Update()
+    {
+        // In Update, you can check for the player transform more efficiently.
+        if (playerTransform == null)
+        {
+            FindAndSetPlayer();
+        }
+    }
+
+    private void FindAndSetPlayer()
+    {
+        // Find the player GameObject with the "Player" tag.
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+
+        foreach (GameObject p in players)
+        {
+            NetworkObject networkObject = p.GetComponent<NetworkObject>();
+
+            if (networkObject != null && networkObject.OwnerClientId == OwnerClientId)
+            {
+                // Set the playerTransform if it's owned by the local client.
+                playerTransform = p.transform;
+                break;
+            }
+        }
     }
 }
--- Assets/Scripts/Player/shootingWithRaycasts.cs
+++ Assets/Scripts/Player/shootingWithRaycasts.cs
@@ -1,23 +1,51 @@
+
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class shootingWithRaycasts : MonoBehaviour
 {
+    public Transform firePoint;
+    public ParticleSystem muzzleFlash;
+
+    public ParticleSystem impactEffect;
+
+    public TrailRenderer tracerEffect;
     public float damage = 10f;
-    public float range = 100f;
+    public float range = float.MaxValue;
 
     public Camera fpsCam;
+
+    public float fireRate = 15f;
+
+    private bool canShoot = true;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButton("Fire1"))
         {
-            Shoot();
-        }
+            if (canShoot)
+            {
+                Shoot();
+                canShoot = false;
+                StartCoroutine(ShootDelay());
+            }
+        } 
+    }
+
+    IEnumerator ShootDelay()
+    {
+        yield return new WaitForSeconds(1f / fireRate);
+        canShoot = true;
     }
 
     void Shoot ()
     {
+        if (muzzleFlash != null)
+        {
+            muzzleFlash.Play();
+        }
         // Creating a raycast
         RaycastHit hit;
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
@@ -30,6 +58,26 @@ public class shootingWithRaycasts : MonoBehaviour
             {
                 target.TakeDamage(damage);
             }
+
+            TrailRenderer trail = Instantiate(tracerEffect, firePoint.position, Quaternion.identity);
+            StartCoroutine(SpownTrail(trail, hit.point));
+        }
+
+        IEnumerator SpownTrail(TrailRenderer trail, Vector3 hitPoint)
+        {
+            float time = 0f;
+            Vector3 startPosition = trail.transform.position;
+            while (time < 1f)
+            {
+                trail.transform.position = Vector3.Lerp(startPosition, hitPoint, time);
+                time += Time.deltaTime / trail.time;
+                yield return null;
+            }
+            trail.transform.position = hitPoint;
+            ParticleSystem newImpactEffect = Instantiate(impactEffect, hitPoint, Quaternion.LookRotation(hit.normal));
+
+            Destroy(trail.gameObject, trail.time);
+            Destroy(newImpactEffect, 1f);
         }
     }
 }
