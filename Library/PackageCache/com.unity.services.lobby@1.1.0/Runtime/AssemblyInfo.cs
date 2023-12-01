9f9caafbf5ad3320f9f8a0a47121699c7264412e
Wiktor Ziędalski <wiktor.karol@outlook.com>
2023-12-01 15:52:54 +0100

    Remove Library and UserSettings

--- Library/PackageCache/com.unity.services.lobby@1.1.0/Runtime/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Unity.Services.Lobbies.Tests")]
-[assembly: InternalsVisibleTo("Unity.Services.Lobbies.EditorTests")]
-[assembly: InternalsVisibleTo("Unity.Services.Lobby.IntegrationTests")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")] // For Moq
